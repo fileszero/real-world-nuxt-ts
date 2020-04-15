5a55f3d1b7cfa381e488d277c43211d51694e71d
FILES=0 <files.eq.zero@gmail.com>
2020-04-15 15:29:55 +0900

    event description

--- pages/event/_id.vue
+++ pages/event/_id.vue
@@ -1,25 +1,43 @@
 <template>
   <div>
-    <h1>Event # {{ id }}</h1>
+    <h1>{{ event.title }}</h1>
   </div>
 </template>
 <script lang="ts">
+import { Context } from '@nuxt/types'
 import { Vue, Component } from 'nuxt-property-decorator'
 @Component<Index>({
   head() {
     return {
-      title: 'Event #' + this.id,
+      title: this.event.title,
       meta: [
         {
           hid: 'description',
           name: 'description', // <-- for our meta description tag
-          content: 'What you need to know about event #' + this.id
+          content: 'What you need to know about ' + this.event.title
         }
       ]
+    }
+  },
+  async asyncData(context: Context) {
+    console.log('index.vue asyncData')
+    // https://axios.nuxtjs.org/setup.html#typescript
+    try {
+      const response = await context.$axios.get('http://localhost:3000/events/' + context.params.id)
+      console.log('index.vue then')
+      return {
+        event: response.data
+      }
+    } catch (e) {
+      context.error({
+        statusCode: 503,
+        message: 'unable to fetch this event#' + context.params.id
+      })
     }
   }
 })
 export default class Index extends Vue {
+  event: any
   get id() {
     return this.$route.params.id
   }
