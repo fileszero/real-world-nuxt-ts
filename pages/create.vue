6aa1a4196a8c29157b41a3e65d905bb955348cd0
FILES=0 <files.eq.zero@gmail.com>
2020-04-15 09:14:09 +0900

    add vue-meta part

--- pages/create.vue
+++ pages/create.vue
@@ -3,3 +3,21 @@
     <h1>Create an Event</h1>
   </div>
 </template>
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+@Component({
+  head() {
+    return {
+      title: 'Create an Event - Real World Events',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description', // <-- for our meta description tag
+          content: 'You can create the event in your neighborhood'
+        }
+      ]
+    }
+  }
+})
+export default class Index extends Vue {}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -3,3 +3,22 @@
     <h1>Events</h1>
   </div>
 </template>
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+@Component({
+  head() {
+    return {
+      title: 'Event Listing - Real World Events',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description', // <-- for our meta description tag
+          content:
+            'Where you can find all the events taking place in your neighborhood'
+        }
+      ]
+    }
+  }
+})
+export default class Index extends Vue {}
+</script>
